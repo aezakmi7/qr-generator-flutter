654055d73b6c8fd2f14d90b687367010c1cb4ebd
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-07-29 05:49:06 -0400

    init commit

---
+++ android/app/src/main/kotlin/com/example/qr_generator_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.qr_generator_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR Code Generator'),
+      ),
+      body: Center(
+        child: Text('QR Code Generator'),
+      ),
+    );
+  }
+}
