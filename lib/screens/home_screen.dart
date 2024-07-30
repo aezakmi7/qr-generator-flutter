68382cc43143f85708d010eddd0575ff601eab7c
Mykola Herasymenko <nicksuper2000@gmail.com>
2024-07-30 06:11:05 -0400

    Design and QR code logic [FINAL]

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_generator_flutter/utilities/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,45 +15,103 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR Code Generator'),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              qrData.isEmpty
-                  ? SizedBox(
-                      height: 200,
-                      child: Center(
-                          child: Text('Enter your data to generate QR code')))
-                  : QrImageView(
-                      data: qrData,
-                      version: QrVersions.auto,
-                      size: 200.0,
-                    ),
-              SizedBox(height: 50),
-              SizedBox(
-                width: 300,
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Enter your data here',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
+      backgroundColor: appGrey,
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Transform.translate(
+              offset: Offset(0, -110),
+              child: QrImageView(
+                backgroundColor: Colors.white,
+                data: qrData,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    onChanged: (value) {
+                      qrData = value;
+                    },
+                    style: TextStyle(color: Colors.white),
+                    cursorColor: appYellow,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      prefixIcon: Icon(Icons.qr_code),
+                      prefixIconColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: appYellow, width: 3.0),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      label: Text(
+                        'Data',
                       ),
                     ),
                   ),
-                  onChanged: (value) {
+                ),
+                const SizedBox(height: 20),
+                const SizedBox(
+                  width: 300,
+                  child: Text(
+                    'Please type in your data you want to generate',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
                     setState(() {
-                      qrData = value;
+                      setState(() {});
+                      FocusScope.of(context).requestFocus(FocusNode());
                     });
                   },
+                  icon: Icon(
+                    Icons.qr_code,
+                    color: appGrey,
+                  ),
+                  label: Text(
+                    'Generate QR Code',
+                    style: TextStyle(color: appGrey),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                      appYellow,
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    fixedSize: const WidgetStatePropertyAll(
+                      Size(280, 55),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 50), // add some space at the bottom
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
